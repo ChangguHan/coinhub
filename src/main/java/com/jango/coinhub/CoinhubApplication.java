562ef0fe7e936da2f1b6e808c0b322d61c136677
changgu.han <changgu.han@linecorp.com>
2022-10-19 23:26:24 +0900

    init

---
+++ src/main/java/com/jango/coinhub/CoinhubApplication.java
@@ -0,0 +1,13 @@
+package com.jango.coinhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoinhubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jango/coinhub/CoinhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jango.coinhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
